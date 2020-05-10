7da1cfc3c4fae3da1398a22fbb9057c43a64d5bf
RuslanZinchenko <zinchenko.rusya@gmail.com>
2020-05-10 16:45:42 +0300

    calculator

---
+++ src/components/Articles/Articles.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Articles.module.css';
+
+const Articles = () => (
+  <div className={styles.articlesWrapper}>
+    <h2 className={styles.articles}>Articles</h2>
+  </div>
+);
+
+export default Articles;
---
+++ src/components/ContactUs/ContactUs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './ContactUs.module.css';
+
+const ContactUs = () => (
+  <div className={styles.contactUsWrapper}>
+    <h2 className={styles.contactUs}>Contact us</h2>
+  </div>
+);
+
+export default ContactUs;
